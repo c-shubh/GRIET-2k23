5bbddb4ba8d404b488954009b39999979ddd2ad8
Shubh Chudasama <shubh.cs@proton.me>
2023-01-22 16:10:27 +0530

    replace body-parser with express built-in methods

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     ecmaVersion: "latest",
   },
   rules: {
-    indent: ["error", 2],
+    indent: ["error", 2, { SwitchCase: 1 }],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
--- server/app.js
+++ server/app.js
@@ -1,16 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = 3000;
-const bodyParser = require("body-parser");
 
-app.use(bodyParser.json());
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use("/api", require("./routes/apiRoutes.js"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port ${process.env.PORT}`);
 });
