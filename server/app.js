eb610da06cdd6c6596f2b275076a0283cf920037
0xvikasrushi <0xvikas@gmail.com>
2023-01-22 15:03:04 +0530

    added dev scripts

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
 
 app.use("/api", require("./routes/apiRoutes.js"));
 
