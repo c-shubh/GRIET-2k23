fb810d8a1282e82a341255ee261a55abd5e848c5
0xvikasrushi <0xvikas@gmail.com>
2023-01-23 21:47:39 +0530

    changePassword updated

--- server/controller/changepassword.controller.js
+++ server/controller/changepassword.controller.js
@@ -1,34 +1,25 @@
 const Student = require("./../models/student.js");
 const Teacher = require("./../models/teacher.js");
-const Class = require("./../models/class.js");
 const bcrypt = require("bcrypt");
 
 const changePassword = async (req, res) => {
-  console.log("hi");
   const { type, id, currentPassword, updatedPassword } = req.body;
   const searchQuery = type === "Student" ? { rollNo: id } : { teacherID: id };
-  let data;
 
+  let data;
   if (type === "Student") data = await Student.find(searchQuery);
   else data = await Teacher.find(searchQuery);
-  console.log(data[0].password + " " + currentPassword);
-  var result = await bcrypt.compare(currentPassword, data[0].password);
 
+  var result = await bcrypt.compare(currentPassword, data[0].password);
+  console.log(data);
   if (!result) {
     return res.status(401).json({ error: "Incorrect current password" });
   } else {
+    let DB = type === "Student" ? Student : Teacher;
     const updatedPasswordhash = await bcrypt.hash(updatedPassword, 10);
-    console.log(updatedPassword);
-    //  const currentPasswordhash = await bcrypt.hash(currentPassword, 10);
-    console.log(updatedPasswordhash);
-    if (type === "Student") {
-      Student.updateOne({ rollNo: id }, { $set: { password: updatedPasswordhash } }, { new: true }, (err, doc) => {
-        res.send(doc);
-      });
-    } else {
-      Teacher.updateOne({ password: currentPasswordhash }, { $set: { password: updatedPasswordhash } }, () => {});
-      res.status(200).json({ msg: "successfully teacher password changed" });
-    }
+    DB.updateOne(searchQuery, { $set: { password: updatedPasswordhash } }, (err, doc) => {});
+    res.status(200).json({ msg: "successfully teacher password changed" });
   }
 };
+
 module.exports = { changePassword };
