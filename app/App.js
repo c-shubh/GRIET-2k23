53b58a38ad2ff91b04f2a4807ad1470ce6d925b1
Hitesh Lalwani <lwni@protonmail.com>
2023-01-26 17:22:11 +0530

    structure

--- app/App.js
+++ app/App.js
@@ -2,30 +2,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Teacher from "./teacher";
-import Student from "./student";
 import { useEffect } from "react";
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Button
-        title="Teacher"
-        onPress={() => navigation.navigate("teacher")}
-      ></Button>
-
-      <Button
-        title="Student"
-        onPress={() => navigation.navigate("student")}
-      ></Button>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+  /*
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="home">
@@ -36,13 +24,5 @@ export default function App() {
       </Stack.Navigator>
     </NavigationContainer>
   );
+  */
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
