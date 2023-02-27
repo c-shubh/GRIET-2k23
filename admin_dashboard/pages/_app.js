6220eb27092da9e738cd50630aa0db960ed52ea3
0xvikasrushi <0xvikas@gmail.com>
2023-02-27 14:55:49 +0530

    ui added

--- admin_dashboard/pages/_app.js
+++ admin_dashboard/pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+// import "../styles/globals.css";
+
+import * as React from "react";
+
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- admin_dashboard/pages/index.js
+++ admin_dashboard/pages/index.js
@@ -1,28 +1,45 @@
-import React, { useState, useEffect } from "react";
-import { Table, Input, Select } from "antd";
-
-
+import { Table, Select } from "antd";
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Flex,
+  Center,
+  Heading,
+  Spacer,
+  Input,
+  Tag,
+} from "@chakra-ui/react";
+import { RotatingLines } from "react-loader-spinner";
 
 const AttendanceTable = () => {
   const [attendanceData, setAttendanceData] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [filters, setFilters] = useState({
     studentRollNo: "",
     teacherID: "",
-    classID: ""
+    classID: "",
   });
 
   useEffect(() => {
-    fetch("/api/hello")
-      .then(res => res.json())
-      .then(data => setAttendanceData(data));
+    fetchData();
   }, []);
 
-  const handleFilterChange = e => {
+  const fetchData = async () => {
+    setLoading(true);
+    const req = await fetch("/api/hello");
+    const res = await req.json();
+    setAttendanceData(res);
+
+    setLoading(false);
+  };
+
+  const handleFilterChange = (e) => {
     const { name, value } = e.target;
     setFilters({ ...filters, [name]: value });
   };
 
-  const filteredData = attendanceData.filter(attendance => {
+  console.log(attendanceData);
+  const filteredData = attendanceData.filter((attendance) => {
     return (
       attendance.studentRollNo.includes(filters.studentRollNo) &&
       attendance.teacherID.includes(filters.teacherID) &&
@@ -60,34 +77,90 @@ const AttendanceTable = () => {
       title: "Present",
       dataIndex: "present",
       key: "present",
-      render: present => present ? "Yes" : "No"
-    }
+      render: (present) =>
+        present ? (
+          <Tag color="green" key={present}>
+            {" "}
+            Present
+          </Tag>
+        ) : (
+          <Tag color="red" key={present}>
+            Absent
+          </Tag>
+        ),
+    },
   ];
 
   return (
-    <div>
-      <Input
-        placeholder="Student Roll No"
-        name="studentRollNo"
-        value={filters.studentRollNo}
-        onChange={handleFilterChange}
-      />
-      <Input
-        placeholder="Teacher ID"
-        name="teacherID"
-        value={filters.teacherID}
-        onChange={handleFilterChange}
-      />
-      <Input
-        placeholder="Class ID"
-        name="classID"
-        value={filters.classID}
-        onChange={handleFilterChange}
-      />
-      <Table dataSource={filteredData} columns={columns} />
+    <>
+      {loading ? (
+        <Center display="flex" m="300">
+          <RotatingLines
+            strokeColor="grey"
+            strokeWidth="5"
+            animationDuration="0.75"
+            width="120"
+            visible={true}
+          />
+        </Center>
+      ) : (
+        <>
+          <Flex
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            minHeight="100vh"
+            background="gray.100"
+          >
+            <Box
+              width="100%"
+              maxWidth="900px"
+              p="4"
+              background="white"
+              borderRadius="md"
+              boxShadow="md"
+            >
+              <Flex alignItems="center" gap="4" mb="4">
+                <Box p="2">
+                  <Heading size="md" color="blue.700">
+                    Admin Dashboard
+                  </Heading>
+                </Box>
+                <Spacer />
 
-    </div>
-  ) ;
+                <Box>
+                  <Input
+                    placeholder="Student Roll No"
+                    color="black"
+                    name="studentRollNo"
+                    value={filters.studentRollNo}
+                    onChange={handleFilterChange}
+                  />
+                </Box>
+                <Box>
+                  <Input
+                    placeholder="Teacher ID"
+                    name="teacherID"
+                    value={filters.teacherID}
+                    onChange={handleFilterChange}
+                  />
+                </Box>
+                <Box>
+                  <Input
+                    placeholder="Class ID"
+                    name="classID"
+                    value={filters.classID}
+                    onChange={handleFilterChange}
+                  />
+                </Box>
+              </Flex>
+              <Table dataSource={filteredData} columns={columns} />
+            </Box>
+          </Flex>
+        </>
+      )}
+    </>
+  );
 };
 
 export default AttendanceTable;
